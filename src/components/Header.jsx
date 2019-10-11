de160a4ebfaa08be5c2df32ab24f8849f2051113
Andrey Grach <andrejgrach@icloud.com>
2019-10-11 10:42:09 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://sun9-11.userapi.com/c850328/v850328506/132c98/F1ncE73i7wY.jpg' />
+    </header>;
+}
+
+export default Header;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg'/>
+        </div>
+        <div>
+            ava + desc
+        </div>
+
+        <div>
+            My posts
+
+            <div>
+                New post
+            </div>
+
+            <div>
+                <div>
+                    Post1
+                </div>
+
+                <div>
+                    Post2
+                </div>
+
+            </div>
+        </div>
+    </div>
+}
+
+    export default Profile;
